c86dffa70745eb68024151ec393fcb14968cf785
Valeriy Osipov <info@vosipov.com>
2016-08-26 16:04:04 +0300

    Add project files.

---
+++ redfoodie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(redfoodie.Startup))]
+namespace redfoodie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
