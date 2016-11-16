8346cc6ae988a0b966b44d4e65ac9ffff7b28bc9
Valeriy Osipov <info@vosipov.com>
2016-11-17 00:44:40 +0300

    Add Changing Password

--- redfoodie/Controllers/ManageController.cs
+++ redfoodie/Controllers/ManageController.cs
@@ -51,17 +51,8 @@ namespace redfoodie.Controllers
 
         //
         // GET: /Manage/ProfileSettings
-        public async Task<ActionResult> ProfileSettings(ManageMessageId? message)
+        public async Task<ActionResult> ProfileSettings()
         {
-//            ViewBag.StatusMessage =
-//                message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
-//                : message == ManageMessageId.SetPasswordSuccess ? "Your password has been set."
-//                : message == ManageMessageId.SetTwoFactorSuccess ? "Your two-factor authentication provider has been set."
-//                : message == ManageMessageId.Error ? "An error has occurred."
-//                : message == ManageMessageId.AddPhoneSuccess ? "Your phone number was added."
-//                : message == ManageMessageId.RemovePhoneSuccess ? "Your phone number was removed."
-//                : "";
-//
             var userId = User.Identity.GetUserId();
             var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
             var model = new ProfileSettingsViewModel
@@ -73,10 +64,7 @@ namespace redfoodie.Controllers
                 Facebook = user.Facebook,
                 Website = user.Website,
                 Bio = user.Bio,
-                PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
-//                TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
-//                Logins = await UserManager.GetLoginsAsync(userId)
-//                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
+                PhoneNumber = await UserManager.GetPhoneNumberAsync(userId)
             };
             return View(model);
         }
@@ -85,21 +73,26 @@ namespace redfoodie.Controllers
         [ValidateAntiForgeryToken]
         public async Task<JsonResult> ProfileSettings(ProfileSettingsViewModel model)
         {
-            if (!ModelState.IsValid)
-                return Json(JsonResponseFactory.ErrorResponse(ModelState.Where(pair => pair.Value.Errors.Count > 0)
+            if (ModelState.IsValid)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                user.UserName = model.UserName;
+                user.CityId = model.SelectedCity;
+                user.Twitter = model.Twitter;
+                user.Facebook = model.Facebook;
+                user.Website = model.Website;
+                user.PhoneNumber = model.PhoneNumber;
+                user.Bio = model.Bio;
+                var updateResult = await UserManager.UpdateAsync(user);
+
+                if (updateResult.Succeeded)
+                {
+                    return Json(JsonResponseFactory.SuccessResponse());
+                }
+                AddErrors(updateResult);
+            }
+            return Json(JsonResponseFactory.ErrorResponse(ModelState.Where(pair => pair.Value.Errors.Count > 0)
                     .ToDictionary(pair => pair.Key, pair => pair.Value.Errors.Select(error => error.ErrorMessage))));
-            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-            user.UserName = model.UserName;
-            user.CityId = model.SelectedCity;
-            user.Twitter = model.Twitter;
-            user.Facebook = model.Facebook;
-            user.Website = model.Website;
-            user.PhoneNumber = model.PhoneNumber;
-            user.Bio = model.Bio;
-            var updateResult = await UserManager.UpdateAsync(user);
-            return Json(updateResult.Succeeded
-                ? JsonResponseFactory.SuccessResponse()
-                : JsonResponseFactory.ErrorResponse(updateResult.Errors));
         }
 
         public ActionResult ViewProfile()
@@ -110,79 +103,17 @@ namespace redfoodie.Controllers
         public ActionResult InviteFriends()
         {
             return View();
-        }
-
-        //
-        // POST: /Manage/RemoveLogin
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> RemoveLogin(string loginProvider, string providerKey)
-        {
-            ManageMessageId? message;
-            var result = await UserManager.RemoveLoginAsync(User.Identity.GetUserId(), new UserLoginInfo(loginProvider, providerKey));
-            if (result.Succeeded)
-            {
-                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-                if (user != null)
-                {
-                    await SignInManager.SignInAsync(user, false, false);
-                }
-                message = ManageMessageId.RemoveLoginSuccess;
-            }
-            else
-            {
-                message = ManageMessageId.Error;
-            }
-            return RedirectToAction("ManageLogins", new { Message = message });
-        }
-
-        //
-        // GET: /Manage/ChangePassword
-        public ActionResult ChangePassword()
-        {
-            return View();
-        }
+        }        
 
         //
         // POST: /Manage/ChangePassword
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> ChangePassword(ChangePasswordViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-            var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
-            if (result.Succeeded)
-            {
-                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-                if (user != null)
-                {
-                    await SignInManager.SignInAsync(user, false, false);
-                }
-                return RedirectToAction("ProfileSettings", new { Message = ManageMessageId.ChangePasswordSuccess });
-            }
-            AddErrors(result);
-            return View(model);
-        }
-
-        //
-        // GET: /Manage/SetPassword
-        public ActionResult SetPassword()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Manage/SetPassword
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> SetPassword(SetPasswordViewModel model)
+        public async Task<JsonResult> ChangePassword(ChangePasswordViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var result = await UserManager.AddPasswordAsync(User.Identity.GetUserId(), model.NewPassword);
+                var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
                 if (result.Succeeded)
                 {
                     var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
@@ -190,59 +121,12 @@ namespace redfoodie.Controllers
                     {
                         await SignInManager.SignInAsync(user, false, false);
                     }
-                    return RedirectToAction("ProfileSettings", new { Message = ManageMessageId.SetPasswordSuccess });
+                    return Json(JsonResponseFactory.SuccessResponse());
                 }
                 AddErrors(result);
             }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
-        //
-        // GET: /Manage/ManageLogins
-        public async Task<ActionResult> ManageLogins(ManageMessageId? message)
-        {
-            ViewBag.StatusMessage =
-                message == ManageMessageId.RemoveLoginSuccess ? "The external login was removed."
-                : message == ManageMessageId.Error ? "An error has occurred."
-                : "";
-            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-            if (user == null)
-            {
-                return View("Error");
-            }
-            var userLogins = await UserManager.GetLoginsAsync(User.Identity.GetUserId());
-            var otherLogins = AuthenticationManager.GetExternalAuthenticationTypes().Where(auth => userLogins.All(ul => auth.AuthenticationType != ul.LoginProvider)).ToList();
-            ViewBag.ShowRemoveButton = user.PasswordHash != null || userLogins.Count > 1;
-            return View(new ManageLoginsViewModel
-            {
-                CurrentLogins = userLogins,
-                OtherLogins = otherLogins
-            });
-        }
-
-        //
-        // POST: /Manage/LinkLogin
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult LinkLogin(string provider)
-        {
-            // Request a redirect to the external login provider to link a login for the current user
-            return new AccountController.ChallengeResult(provider, Url.Action("LinkLoginCallback", "Manage"), User.Identity.GetUserId());
-        }
-
-        //
-        // GET: /Manage/LinkLoginCallback
-        public async Task<ActionResult> LinkLoginCallback()
-        {
-            var loginInfo = await AuthenticationManager.GetExternalLoginInfoAsync(XsrfKey, User.Identity.GetUserId());
-            if (loginInfo == null)
-            {
-                return RedirectToAction("ManageLogins", new { Message = ManageMessageId.Error });
-            }
-            var result = await UserManager.AddLoginAsync(User.Identity.GetUserId(), loginInfo.Login);
-            return result.Succeeded ? RedirectToAction("ManageLogins") : RedirectToAction("ManageLogins", new { Message = ManageMessageId.Error });
+            return Json(JsonResponseFactory.ErrorResponse(ModelState.Where(pair => pair.Value.Errors.Count > 0)
+                .ToDictionary(pair => pair.Key, pair => pair.Value.Errors.Select(error => error.ErrorMessage))));
         }
 
         protected override void Dispose(bool disposing)
@@ -269,21 +153,6 @@ namespace redfoodie.Controllers
                 ModelState.AddModelError("", error);
             }
         }
-
-        private bool HasPassword()
-        {
-            var user = UserManager.FindById(User.Identity.GetUserId());
-            return user?.PasswordHash != null;
-        }
-
-        public enum ManageMessageId
-        {
-            ChangePasswordSuccess,
-            SetPasswordSuccess,
-            RemoveLoginSuccess,
-            Error
-        }
-
 #endregion
     }
 }
