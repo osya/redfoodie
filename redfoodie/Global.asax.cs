89809a72516dfb11070b3b44d2f61ce8a70ed17a
Valeriy Osipov <info@vosipov.com>
2017-01-11 00:36:51 +0300

    Add simple logging for AppHarbor

--- redfoodie/Global.asax.cs
+++ redfoodie/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using redfoodie.Models;
+using System.Web.Management;
 
 namespace redfoodie
 {
@@ -18,10 +19,19 @@ namespace redfoodie
 
         protected void Application_Start()
         {
+            new LogEvent("message to myself").Raise();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+
+    public class LogEvent : WebRequestErrorEvent
+    {
+        public LogEvent(string message)
+            : base(null, null, 100001, new Exception(message))
+        {
         }
     }
 }
