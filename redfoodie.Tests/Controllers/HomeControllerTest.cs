f58091216413caa058de408f6b0937ece04851b9
Valeriy Osipov <info@vosipov.com>
2017-01-12 10:00:12 +0300

    Uncomment some test code

--- redfoodie.Tests/Controllers/HomeControllerTest.cs
+++ redfoodie.Tests/Controllers/HomeControllerTest.cs
@@ -35,16 +35,16 @@ namespace redfoodie.Tests.Controllers
             }
         }
 
-        //        private class MockHttpSession : HttpSessionStateBase
-        //        {
-        //            private readonly Dictionary<string, object> _sessionDictionary = new Dictionary<string, object>();
-        //
-        //            public override object this[string name]
-        //            {
-        //                get { return _sessionDictionary[name]; }
-        //                set { _sessionDictionary[name] = value; }
-        //            }
-        //        }
+        private class MockHttpSession : HttpSessionStateBase
+        {
+            private readonly Dictionary<string, object> _sessionDictionary = new Dictionary<string, object>();
+        
+            public override object this[string name]
+            {
+                get { return _sessionDictionary[name]; }
+                set { _sessionDictionary[name] = value; }
+            }
+        }
 
         //        private static ApplicationDbContext Db {
         //            get
